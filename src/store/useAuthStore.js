6990fac47974e7eddb7e3599cce8b896aa50b149
Tanvir Anjum <tanviranjum010@gmail.com>
2025-12-06 01:57:03 +0600

    holla

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
